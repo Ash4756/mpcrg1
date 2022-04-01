b07f25cf2454360042c0387496ad1a6d21799d28
Ash4756 <86113463+Ash4756@users.noreply.github.com>
2022-04-01 13:37:31 +0530

    Add files via upload

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
-# mpcrg1
+# asynchronousBallMovement
+Boiler plate for ball moving aysnchronously
---
+++ sketch.js
@@ -0,0 +1,45 @@
+var database,ball,position;
+function setup()
+{
+    createCanvas(800,700);
+    database=firebase.database();
+    ball=createSprite(400,300,20,20);
+    ball.shapeColor="red";
+    var dbref=database.ref("ball/position");
+    dbref.on("value",readdb);
+}
+function draw()
+{
+    background("lightblue");
+
+    if(keyDown(LEFT_ARROW))
+    {
+        writedb(-1,0);
+    }
+    else if(keyDown(RIGHT_ARROW))
+    {
+        writedb(1,0);
+    }
+    else if(keyDown(UP_ARROW))
+    {
+        writedb(0,-1);
+    }
+    else if(keyDown(DOWN_ARROW))
+    {
+        writedb(0,+1);
+    }
+
+    drawSprites();
+}
+function readdb(data)
+{
+  position = data.val();
+  ball.x = position.x;
+  ball.y = position.y;
+}
+
+function writedb(x,y)
+{
+  var dbref = database.ref("ball/position");
+  dbref.set({'x':position.x+x,'y':position.y+y});
+}
